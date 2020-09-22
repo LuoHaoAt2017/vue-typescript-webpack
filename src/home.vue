27f73a07b28101023ff0ba3a383f0e0853258111
luohao <luohao@authine.com>
2020-09-22 11:07:48 +0800

    chore: 添加icon图标库

--- src/home.vue
+++ src/home.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="home">
-    <a-button>{{ msg }}</a-button>
+    <!-- <a-button>{{ msg }}</a-button> -->
+    <i class="fa fa-spinner fa-spin"></i>
+    <i class="fa fa-circle-o-notch fa-spin"></i>
+    <i class="fa fa-refresh fa-spin"></i>
+    <i class="fa fa-cog fa-spin"></i>
+    <i class="fa fa-spinner fa-pulse"></i>
   </div>
 </template>
 
